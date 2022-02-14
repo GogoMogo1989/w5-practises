c530b15bf8efeb0766d9e5b3c8cd27b171ed6a4a
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-02-14 20:33:16 +0100

    worked

---
+++ frontend/script.js
@@ -0,0 +1,125 @@
+/* console.log(a);
+console.log(c);
+ */
+//console.log("Hello");
+//console.log('Sima');
+//console.log(`Backt
+//ck`);
+
+
+//console.log(typeof 2)
+//console.log(typeof NaN);
+//console.log(0/0);
+//console.log(2.14);
+
+//console.log(true);
+//console.log(false);
+
+//console.log(undefined);
+//console.log(null);
+
+//console.log(typeof undefined);
+//console.log(typeof null);
+
+/*     console.log(
+    {
+    "title": "Start Coding",
+    "type": "solo",
+    "difficulty": 4.1,
+    "usefullnes": 3.2,
+    "enjoyability":3.7,
+    "materials": 3.4,
+    "tasks": [
+        {
+            "title": "Bank accounts",
+            "description": ""
+        },
+        {
+            "title": "100 doors",
+            "description": ""
+        },
+        {
+            "title": "World filter",
+            "description": ""
+        }
+    ]
+}
+) */
+
+/* var a= "Hello1";
+let b= "Hello2";
+const c= "Hello3";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+ */
+
+
+/* function d() {
+    if(1+1 === 2){
+       
+    }
+    var a= "Hello1"
+    let b= "Hello2"
+    const c= "Hello3"
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+d()
+
+ */
+
+/* let e=1
+console.log(e)
+
+e=2
+console.log(e)
+
+e=e*2
+console.log(e) */
+
+
+
+
+/* function multiplyByTwo(num){
+    console.log(num)
+    return num*2
+}
+
+let f = multiplyByTwo(1)
+console.log(f)
+console.log(multiplyByTwo(1))
+console.log(num)
+
+ */
+
+/* let g={
+    myMehtod: function (){
+        console.log("Hello")
+    }
+}
+ */
+/* g.myMehtod()
+
+let window = { Ez enm fog működni, csak példa
+    addEventListener: function (eventName, eventFunction){
+        if(eventName === "load"){
+            eventFunction()
+        }
+    }
+}
+window.addEventListener("load",loadEvent) */
+
+let h = {
+    anOtherMethod: function(text){
+        return `You sent following argument to this method: ${text}`
+    }
+}
+
+let i= h.anOtherMethod("Hello")
+console.log(i);
+console.log(h.anOtherMethod("An other argument"));
